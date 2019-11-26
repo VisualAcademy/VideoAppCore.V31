80791d70c47f5ca73d13156046a57bb16ca71980
RedPlus <redplus@redplus.net>
2019-11-26 12:27:01 +0900

    비디오 앱을 위한 Videos 테이블 생성 및 로컬 DB에 게시

---
+++ VideoAppCore.SqlServer/Videos.sql
@@ -0,0 +1,14 @@
+﻿-- 비디오 테이블
+CREATE TABLE [dbo].[Videos]
+(
+	[Id] Int Not Null Identity(1, 1) Primary Key,
+	[Created] DateTimeOffset(7) 
+		Default(SysDateTimeOffset() AT TIME ZONE 'Korea Standard Time'),
+
+	[Title] NVarChar(255) Not Null,			-- 제목
+	[Url] NVarChar(Max) Null,				-- URL
+
+	[Name] NVarChar(50) Null,				-- 이름
+	[Company] NVarChar(255) Null,			-- 회사
+)
+Go
