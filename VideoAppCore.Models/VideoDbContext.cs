0ac2e300a7b950f1318db1251c35d09be3524a94
RedPlus <redplus@redplus.net>
2019-11-27 01:57:02 +0900

    VideoDbContext 사용 테스트 레이저 컴포넌트 페이지 작성

--- VideoAppCore.Models/VideoDbContext.cs
+++ VideoAppCore.Models/VideoDbContext.cs
@@ -25,7 +25,7 @@ namespace VideoAppCore.Models
             if (!optionsBuilder.IsConfigured)
             {
                 string connectionString = ConfigurationManager.ConnectionStrings[
-                    "ConnectionString"].ConnectionString;
+                    "DefaultConnection"].ConnectionString;
                 optionsBuilder.UseSqlServer(connectionString); 
             }
         }
